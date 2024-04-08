285044092914ce0504fe9abf65bac61e1bdeaff3
Ronaldo <ronaldojssilva@gmail.com>
2024-04-08 11:43:53 -0300

    Domain Rich Getting Rich Model

--- CleanArch/CleanArch.Domain/Entities/Category.cs
+++ CleanArch/CleanArch.Domain/Entities/Category.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanArch.Domain.Validation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,29 @@ using System.Threading.Tasks;
 
 namespace CleanArch.Domain.Entities
 {
-    public class Category
+    public sealed class Category
     {
-        public int Id { get; set; }
+        public int Id { get; private set; }
         public string Name { get; private set; }
         public ICollection<Product> Products { get; private set; }
 
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value.");
+            Id = id;
+            ValidateDomain(name);
+        }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name. Name is required.");
+            DomainExceptionValidation.When(name.Length < 3, "Invalid name, too short, minimum 3 characters.");
+            Name = name;
+        }
     }
 }
