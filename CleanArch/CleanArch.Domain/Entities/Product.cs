ecdd687a2bbadf86847c1110d0e0d48c44b82844
Ronaldo <ronaldojssilva@gmail.com>
2024-04-08 14:00:10 -0300

    Product Unit Tests

--- CleanArch/CleanArch.Domain/Entities/Product.cs
+++ CleanArch/CleanArch.Domain/Entities/Product.cs
@@ -54,7 +54,7 @@ namespace CleanArch.Domain.Entities
 
             DomainExceptionValidation.When(stock < 0, "Invalid stock value");
 
-            DomainExceptionValidation.When(image.Length > 250,
+            DomainExceptionValidation.When(image?.Length > 250,
                 "Invalid image name, too long, maximum 250 characters");
 
             Name = name;
