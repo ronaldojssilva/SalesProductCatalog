6141688df2dddc221a913cab77a68426c5e1d653
Ronaldo <ronaldojssilva@gmail.com>
2024-04-08 17:29:23 -0300

    Infra IoC Project Registry Services Repositories

---
+++ CleanArch/CleanArch.IoC/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using CleanArch.Domain.Validation;
+using CleanArch.Infra.Data.Context;
+using CleanArch.Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArch.Infra.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+             options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"
+            ), b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            return services;
+        }
+    }
+}
