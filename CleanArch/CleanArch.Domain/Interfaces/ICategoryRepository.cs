c04e0fa4648b18d46b303124641e2117a117b5b0
Ronaldo <ronaldojssilva@gmail.com>
2024-04-08 13:31:11 -0300

    Creating Repository Interfaces

---
+++ CleanArch/CleanArch.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using CleanArch.Domain.Entities;
+
+namespace CleanArch.Domain.Validation
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetCatories();
+        Task<Category> GetById(int? id);
+
+        Task<Category> Create(Category category);
+        Task<Category> Update(Category category);
+        Task<Category> Remove(Category category);
+    }
+}
---
+++ CleanArch/CleanArch.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using CleanArch.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Domain.Validation
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetByIdAsync(int? id);
+
+        Task<Product> GetProductCategoryAsync(int? id);
+
+        Task<Product> CreateAsync(Product product);
+        Task<Product> UpdateAsync(Product product);
+        Task<Product> RemoveAsync(Product product);
+    }
+}
