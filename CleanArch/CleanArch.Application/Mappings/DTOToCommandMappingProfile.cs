5ce764c58704da773637bca8c79053feb086772d
Ronaldo <ronaldojssilva@gmail.com>
2024-04-09 14:01:41 -0300

    Create DTOToCommandMappingProfile.cs

---
+++ CleanArch/CleanArch.Application/Mappings/DTOToCommandMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CleanArch.Application.DTOs;
+using CleanArch.Application.Products.Commands;
+using CleanArch.Domain.Entities;
+
+namespace CleanArch.Application.Mappings
+{
+    public class DTOToCommandMappingProfile : Profile
+    {
+        public DTOToCommandMappingProfile()
+        {
+            CreateMap<ProductDTO, ProductCreateCommand>().ReverseMap();
+            CreateMap<ProductDTO, ProductUpdateCommand>().ReverseMap();
+        }
+    }
+}
