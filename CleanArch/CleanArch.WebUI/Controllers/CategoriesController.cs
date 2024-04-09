cd0821b1f563340bfd9297313c0df5ac29f7c6e4
Ronaldo <ronaldojssilva@gmail.com>
2024-04-09 19:42:17 -0300

    Delete Category

--- CleanArch/CleanArch.WebUI/Controllers/CategoriesController.cs
+++ CleanArch/CleanArch.WebUI/Controllers/CategoriesController.cs
@@ -59,5 +59,26 @@ namespace CleanArch.WebUI.Controllers
             return View(categoryDto);
         }
 
+        [HttpGet()]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var categoryDto = await _categoryService.GetById(id);
+
+            if (categoryDto == null) return NotFound();
+
+            return View(categoryDto);
+        }
+
+        [HttpPost(), ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _categoryService.Remove(id);
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
