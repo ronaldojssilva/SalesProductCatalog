72eb6e3a891ca48c717cb6cba9a87f9b9ee69471
Ronaldo <ronaldojssilva@gmail.com>
2024-04-09 11:02:33 -0300

    CQRS Testing Implementation

--- CleanArch/CleanArch.Application/Interfaces/IProductService.cs
+++ CleanArch/CleanArch.Application/Interfaces/IProductService.cs
@@ -10,11 +10,11 @@ namespace CleanArch.Application.Interfaces
     public interface IProductService
     {
         Task<IEnumerable<ProductDTO>> GetProducts();
-        Task<ProductDTO> GetById(int? id);
+        //Task<ProductDTO> GetById(int? id);
 
-        Task<ProductDTO> GetProductCategory(int? id);
-        Task Add(ProductDTO productDto);
-        Task Update(ProductDTO productDto);
-        Task Remove(int? id);
+        //Task<ProductDTO> GetProductCategory(int? id);
+        //Task Add(ProductDTO productDto);
+        //Task Update(ProductDTO productDto);
+        //Task Remove(int? id);
     }
 }
--- CleanArch/CleanArch.Application/Services/ProductService.cs
+++ CleanArch/CleanArch.Application/Services/ProductService.cs
@@ -2,6 +2,7 @@
 using CleanArch.Application.DTOs;
 using CleanArch.Domain.Entities;
 using CleanArch.Domain.Interfaces;
+using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,14 +14,13 @@ namespace CleanArch.Application.Interfaces
     public class ProductService : IProductService
     {
         private IProductRepository _productRepository;
+        private readonly IMediator _mediator;
 
         private readonly IMapper _mapper;
-        public ProductService(IMapper mapper, IProductRepository productRepository)
+        public ProductService(IMapper mapper, IMediator mediator)
         {
-            _productRepository = productRepository ??
-                 throw new ArgumentNullException(nameof(productRepository));
-
             _mapper = mapper;
+            _mediator = mediator;
         }
 
         public async Task<IEnumerable<ProductDTO>> GetProducts()
@@ -29,35 +29,35 @@ namespace CleanArch.Application.Interfaces
             return _mapper.Map<IEnumerable<ProductDTO>>(productsEntity);
         }
 
-        public async Task<ProductDTO> GetById(int? id)
-        {
-            var productEntity = await _productRepository.GetByIdAsync(id);
-            return _mapper.Map<ProductDTO>(productEntity);
-        }
+        //public async Task<ProductDTO> GetById(int? id)
+        //{
+        //    var productEntity = await _productRepository.GetByIdAsync(id);
+        //    return _mapper.Map<ProductDTO>(productEntity);
+        //}
 
-        public async Task<ProductDTO> GetProductCategory(int? id)
-        {
-            var productEntity = await _productRepository.GetProductCategoryAsync(id);
-            return _mapper.Map<ProductDTO>(productEntity);
-        }
+        //public async Task<ProductDTO> GetProductCategory(int? id)
+        //{
+        //    var productEntity = await _productRepository.GetProductCategoryAsync(id);
+        //    return _mapper.Map<ProductDTO>(productEntity);
+        //}
 
-        public async Task Add(ProductDTO productDto)
-        {
-            var productEntity = _mapper.Map<Product>(productDto);
-            await _productRepository.CreateAsync(productEntity);
-        }
+        //public async Task Add(ProductDTO productDto)
+        //{
+        //    var productEntity = _mapper.Map<Product>(productDto);
+        //    await _productRepository.CreateAsync(productEntity);
+        //}
 
-        public async Task Update(ProductDTO productDto)
-        {
+        //public async Task Update(ProductDTO productDto)
+        //{
 
-            var productEntity = _mapper.Map<Product>(productDto);
-            await _productRepository.UpdateAsync(productEntity);
-        }
+        //    var productEntity = _mapper.Map<Product>(productDto);
+        //    await _productRepository.UpdateAsync(productEntity);
+        //}
 
-        public async Task Remove(int? id)
-        {
-            var productEntity = _productRepository.GetByIdAsync(id).Result;
-            await _productRepository.RemoveAsync(productEntity);
-        }
+        //public async Task Remove(int? id)
+        //{
+        //    var productEntity = _productRepository.GetByIdAsync(id).Result;
+        //    await _productRepository.RemoveAsync(productEntity);
+        //}
     }
 }
